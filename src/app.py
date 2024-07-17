3b4057d7352f40a1ac5bf8eb390e4dca4569d89e
avanegas <arco527@gmail.com>
2024-07-17 10:34:21 -0500

    Add Character(s) route with GET / GET_ID, POST methods

--- src/app.py
+++ src/app.py
@@ -83,6 +83,59 @@ def create_user():
 
 #CRUD characters
 
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+    try:
+        characters = Character.query.all()
+        if not characters:
+            return jsonify({"error":"No characters found"}), 400
+        serialized_characters = [character.serialize() for character in characters]
+        return jsonify({"characters": serialized_characters }), 200
+    except Exception as error:
+        return jsonify({"error": str(error)}), 400
+
+
+@app.route('/character', methods=['POST'])
+def character_create():
+    body = request.json
+
+    character_name = body.get("character_name", None)
+    character_gender = body.get("character_gender", None)
+    hair_color = body.get("hair_color", None)
+    eyes_color = body.get("eyes_color", None)
+
+    required_fields = ["character_name", "character_gender", "hair_color", "eyes_color"]
+
+    for field in required_fields:
+        if field not in body:
+            return jsonify({"error": f"Missing field '{field}'"}),400
+
+    character = Character(character_name=character_name, character_gender=character_gender, hair_color=hair_color, eyes_color=eyes_color)
+
+    try:
+        db.session.add(character)
+        db.session.commit()
+        db.session.refresh(character)
+
+        return jsonify({"message": f"Character {character.character_name} created successfully"}), 201
+
+    except Exception as error:
+        return jsonify({"error": f"{error}"}), 500
+    
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+
+    character = Character.query.get(character_id)
+    if character is None:
+        return jsonify({"error": "Character not found"}), 404
+    
+    return jsonify({
+        "message": f"character: {character} founded successfully"
+    })
+
+
+
 
 
 
