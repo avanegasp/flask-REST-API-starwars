8969f44f6ef807b543985c6ea1a645a828355d6c
avanegas <arco527@gmail.com>
2024-07-22 14:23:57 -0500

    Add PUT methods in Planet model

--- src/planet_routes.py
+++ src/planet_routes.py
@@ -73,6 +73,41 @@ def get_planet(planet_id):
         "message": f"Planet:{planet} founded successfully"
     })
 
+@planet_bp.route('/planet/<int:id>', methods=['PUT'])
+def update_planet(id):
+    body = request.json
+
+    planet = Planet.query.get(id)
+    if planet is None:
+        return jsonify({"error": "Planet not found"}),404
+    
+    required_fields = ["name", "population", "terrain", "surface_water", "gravity"]
+
+    missing_fields = [field for field in required_fields if field not in body]
+    if missing_fields:
+        return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}),400
+    
+    name = body.get("name")
+    population = body.get("population")
+    terrain = body.get("terrain")
+    surface_water = body.get("surface_water")
+    gravity = body.get("gravity")
+
+    planet.name = name
+    planet.population = population
+    planet.terrain = terrain
+    planet.surface_water = surface_water
+    planet.gravity = gravity
+
+    try:
+        db.session.commit()
+        return jsonify({"planet":planet.serialize()}),200
+
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error": str(error)}),500
+
+
 @planet_bp.route('/planet/<int:id>', methods=['DELETE'])
 def deleted_planet(id):
     try:
@@ -87,3 +122,4 @@ def deleted_planet(id):
     except Exception as error:
         db.session.rollback()
         return jsonify({"error": f"{error}"}),500
+    
